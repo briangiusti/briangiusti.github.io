d72eb7be1e93f7efd0df855db723edaf3ab1edd5
Brian Giusti <briangiusti@Brians-MacBook-Pro-2.local>
2023-06-23 17:05:06 -0400

    update config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `brian-giusti`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `briangiusti.github.io`,
+    siteUrl: `https://briangiusti.github.io/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
